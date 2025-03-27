8113126d0b63205c9714e13acc9efe21db9d506c
Nikita Kuzmin <pionerik94@mail.ru>
2025-03-27 12:00:21 +0500

    справил ошибку деления на ноль

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,5 @@
 public class Calculator {
-    private static final String VERSION = "1.0.0";
+    private static final String VERSION = "1.0.1";
 
     public static int add(int a, int b) {
         return a + b;
@@ -9,10 +9,18 @@ public class Calculator {
         return a * b;
     }
 
+    public static int divide(int a, int b) {
+        if (b == 0) {
+            System.out.println("Ошибка! Деление на ноль.");
+            return 0;
+        }
+        return a / b;
+    }
+
     public static void main(String[] args) {
         System.out.println("Калькулятор v" + VERSION);
         System.out.println("Сумма: " + add(5, 3));
         System.out.println("Произведение: " + multiply(5, 3));
+        System.out.println("Деление: " + divide(10, 0));
     }
 }
-
