1fa377f9d1afc8489cdaa36e5f0dfe44f1bc034e
Nikita Kuzmin <pionerik94@mail.ru>
2025-03-27 11:56:02 +0500

    обавил функцию умножения

--- Calculator.java
+++ Calculator.java
@@ -3,7 +3,12 @@ public class Calculator {
         return a + b;
     }
 
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
     public static void main(String[] args) {
         System.out.println("Сумма: " + add(5, 3));
+        System.out.println("Произведение: " + multiply(5, 3));
     }
 }
